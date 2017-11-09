5b565cab9b5d6cbb463e5a1d78e493ed2c3b0b32
Balthazar Caillaud <balthacaillaud@hotmail.fr>
2017-11-09 11:19:26 +0100

    routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  connection.query('insert into infoperso values(null, ?, ?, ?, ?, ?, ?);';)
+  res.render('index');
+});
 module.exports = router;
