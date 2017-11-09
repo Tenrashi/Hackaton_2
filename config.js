b042db18dae44b9f508317219e32e5a7ecc297db
Balthazar Caillaud <balthacaillaud@hotmail.fr>
2017-11-09 15:39:15 +0100

    formulaire+base

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	host     : 'localhost',
+	user     : 'root',
+	password : 'rtg89voj',
+	database : 'hackathon2'
+};
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,47 @@
 var express = require('express');
 var router = express.Router();
-
+const mysql = require('mysql');
+const config = require('../config.js');
+const connection = mysql.createConnection(config);
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
-  connection.query('insert into infoperso values(null, ?, ?, ?, ?, ?, ?, ?);',
-  [req.body.nom, req.body.prenom, req.body.anniversaire, req.body.mail, req.body.num, req.body.place, req.body.arrondissement]
-  function (error, results, fields) {
-					if (error) throw error;
-					res.redirect('/base');
-});
-  res.render('index');
+// router.post('/index', function(req, res, next) {
+//   connection.query('insert into infoperso values(null, ?, ?, ?, ?, ?, ?, ?);',
+//   [req.body.nom, req.body.prenom, req.body.anniversaire, req.body.mail, req.body.num, req.body.place, req.body.arrondissement],
+//   function (error, results, fields) {
+// 				if (error){
+//             console.log(error);
+//         }else{
+//           res.redirect('/base');
+//         };
+//     });
+// });
+//   res.render('index');
+// });
+
+router.post('/index', function (req, res, next) {
+// Création d'article
+connection.query('INSERT INTO infoperso VALUES(NULL, ?, ?, ?, ?, ?, ?, ?);',
+[req.body.nom, req.body.prenom, req.body.anniversaire, req.body.mail, req.body.num, req.body.place, req.body.arrondissement],
+function (error, results, fields) {
+  console.log(req.body);
+    if (error) {
+        console.log(error);
+    } else {
+      res.redirect('/base');
+    };
+  });
 });
 
 router.get('/base', function(req, res, next){
-  connection.query('select * from infoperso')
-  res.render('base');
+    connection.query('SELECT * FROM infoperso;', function(error, results, fields) {
+      res.render('base', {infoperso:results});
+    });
 });
+
+
 module.exports = router;
